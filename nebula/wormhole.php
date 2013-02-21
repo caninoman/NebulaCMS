0b50f96cb5671c7d0ea3f2c2efc31a029143387c
Derek Bourgeois <derek@ibourgeois.com>
2013-02-21 00:16:21 -0500

    Created the wormhole

---
+++ nebula/wormhole.php
@@ -0,0 +1,14 @@
+<?php
+	/*
+	**		NebulaCMS will process all of it's requests through the Wormhole.
+	**		The goal is to connect the 'index.php' file in the site root to 
+	**      the Wormhole, passing the site name through a function located in
+	**      in the Wormhole. 
+	**		
+	**      IE - getSite($site); // Which is to be the only required call in 
+	**		the index file. This will also allow for easy multi-site 
+	**      functionality, as well as sharing content, user base, and other 
+	**      resources.
+	**  
+	*/
+?>
