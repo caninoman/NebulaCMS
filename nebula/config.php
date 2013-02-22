a18e17c3764fc9b72f9255a0bfa331e2be9cc249
Derek Bourgeois <derek@ibourgeois.com>
2013-02-22 00:40:38 -0500

    Created DB Class in config.php to extend PDO Class

--- nebula/config.php
+++ nebula/config.php
@@ -6,11 +6,22 @@
 	
 	// Database Variables
 		
-		$host = 'localhost';
-		$user = 'root';
-		$pass = 'root';
-		$db   = 'nebula';
-		$pre  = 'nebula_';
+		class db extends PDO {
+			
+			private $host = 'localhost';
+			private $user = 'root';
+			private $pass = 'root';
+			private $db   = 'nebula';
+			private $eng  = 'mysql';
+			
+			public function __construct(){ 
+				
+				$dns = $this->engine.':dbname='.$this->db.";host=".$this->host; 
+		        parent::__construct( $dns, $this->user, $this->pass ); 
+		    
+		    } 
+			
+		}
 		
 	// Other Settings
 	
