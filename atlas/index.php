ef78cb1bb9c0eeb464fdcb6af4657a627d4c93eb
Derek Bourgeois <derek@ibourgeois.com>
2013-02-21 20:37:28 -0500

    Updated Atlas

--- atlas/index.php
+++ atlas/index.php
@@ -1,3 +1,37 @@
 <?php
-	echo 'Welcome to the NebulaCMS Atlas.';
+	
+	function page_switch($page){
+		switch($page) {
+			default: include('pages/main.php');
+			break;
+			case 'main': include('pages/main.php');
+			break;
+			case 'test': include('pages/test.php');
+		}
+	}
+	
 ?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+	
+	</head>
+	<body>
+		<div id="header">
+			<h1>Welcome to the NebulaCMS Atlas</h1>
+			<h6>The Official NebulaCMS Guide.</h6>
+		</div>
+		<nav>
+			<ul>
+				<li><a href="?id=main">Main</a></li>
+			</ul>
+		</nav>
+		<div id="content">
+			<?php page_switch($_GET['id']); ?>
+		</div>
+		<div id="footer">
+		
+		</div>
+	</body>
+</html>
