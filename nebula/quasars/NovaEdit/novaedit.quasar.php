a34e257486a52a55e831bb686bacf40abadee609
Derek Bourgeois <derek@ibourgeois.com>
2013-02-22 09:52:28 -0500

    Update nebula/quasars/NovaEdit/novaedit.quasar.php

--- nebula/quasars/NovaEdit/novaedit.quasar.php
+++ nebula/quasars/NovaEdit/novaedit.quasar.php
@@ -1,13 +1,33 @@
 <?php
-	/* NovaEdit 
+	
+    /* NovaEdit 
     
-    class novaEdit{
+    class novaEdit {
 		
-		public function __construct($height, $width, $app, $auth){
+		public function __construct($app, $auth, $height, $width){
 		
+	    	// $app and $auth will allow NovaEdit to determine which tools to make available. 
+	    	if ($app == 'novaComment') {
+	    		
+	    		echo '<div>';
+	    		
+	    		echo '</div>';
+	    		
+	    	}
+		
+		}
+		
+		private function getTools($app, $auth){
 			
+			// Send request for tools to app
+			
+			// Build toolbar
 		
 		}
 		
     }
+    
+    $NovaEdit = new novaEdit;
+    echo $novaEdit->novaEdit('novaComment', 'user', '400', '200');
+
 ?>
