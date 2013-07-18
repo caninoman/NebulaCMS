ee727d2c24e7a1af88ca73d051f5801057e85c6a
Derek Bourgeois <derek@ibourgeois.com>
2013-07-18 14:12:57 -0400

    Create header.php

---
+++ atlas/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>The Official NebulaCMS User Guide | NebulaCMS Atlas</title>
+  </head>
+  <body>
+    <div id="page">
+      <header>
+        <div id="logo">
+          <a href="./">
+            <h1>NebulaCMS | Atlas</h1>
+            <h6>The Official NebulaCMS User Guide</h6>
+          </a>
+        </div> <!-- end of #logo -->
+        <div id="sponsor-top">
+        
+        </div> <!-- end of #sponsor-top -->
+      </header>
