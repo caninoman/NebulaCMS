8e761ec14704e9e083661d9c6fe79faeaec51a5b
Derek Bourgeois <derek@ibourgeois.com>
2013-07-18 14:14:56 -0400

    Create nav.php

---
+++ atlas/nav.php
@@ -0,0 +1,16 @@
+<nav>
+  <ul>
+    <li>
+      <a href="./">Home</a>
+    </li>
+    <li>
+      <a href="./">Beginners</a>
+    </li>
+    <li>
+      <a href="./">Developers</a>
+    </li>
+    <li>
+      <a href="./">Designers</a>
+    </li>
+  </ul>
+</nav>
